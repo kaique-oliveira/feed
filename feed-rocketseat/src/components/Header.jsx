763297b7703f7d67876459b781e59a25ca35b828
Kaique Oliveira Antônio <kaique.oliveira@interfocus.com.br>
2023-06-16 08:23:49 -0300

    Importado o logo SVG e adicionado ao header

--- feed-rocketseat/src/components/Header.jsx
+++ feed-rocketseat/src/components/Header.jsx
@@ -1,11 +1,12 @@
 import styles from './Header.module.css';
 
+import igniteLogo from '../assets/ignite-logo.svg'
+
 export function Header(){
     
     return(
         <header className={styles.header}>
-            <strong>ignite</strong>
+            <img src={igniteLogo} alt='Logotipo ignite'/>
         </header>
-
     );
 }
