3b0884ea8312692ad1733391f3c19cab87308b3c
Kaique Oliveira Antônio <kaique.oliveira@interfocus.com.br>
2023-06-16 16:13:36 -0300

    Ajustado o layout com main e aside

--- feed-rocketseat/src/App.jsx
+++ feed-rocketseat/src/App.jsx
@@ -1,20 +1,30 @@
 import { Header } from "./components/Header";
 import { Post } from "./components/Post";
 
+import styles from './App.module.css';
 import './global.css';
+import { Sidebar } from "./components/SideBar";
 
 export function App() {
   
   return (
     <>   
-      <Header>
-        
-      </Header>
+      <Header/>
+      <div className={styles.wrapper}>
+        <Sidebar/>
 
-      <Post
-        author="Kaique Oliveira"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni dolores expedita voluptatum debitis suscipit inventore veritatis repudiandae exercitationem veniam! Sint perspiciatis similique ullam numquam ipsa officiis consectetur quisquam, aperiam neque."
-      />
+        <main>
+          <Post
+            author="Kaique Oliveira"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni dolores expedita voluptatum debitis suscipit inventore veritatis repudiandae exercitationem veniam! Sint perspiciatis similique ullam numquam ipsa officiis consectetur quisquam, aperiam neque."
+          />
+
+          <Post
+            author="Airon Guimaraes"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni dolores expedita voluptatum debitis suscipit inventore veritatis repudiandae exercitationem veniam! Sint perspiciatis similique ullam numquam ipsa officiis consectetur quisquam, aperiam neque."
+          />
+        </main>
+      </div>
     </>
   )
 }
