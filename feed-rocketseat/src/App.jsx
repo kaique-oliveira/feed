0995ccd90a22367d341da7bffd880e4543108098
Kaique Oliveira Antônio <kaique.oliveira@interfocus.com.br>
2023-06-16 08:38:29 -0300

    Adicionado o componente post na tela

--- feed-rocketseat/src/App.jsx
+++ feed-rocketseat/src/App.jsx
@@ -1,14 +1,22 @@
 import { Header } from "./components/Header";
+import { Post } from "./components/Post";
 
 import './global.css';
 
-function App() {
+export function App() {
   
   return (
     <>   
-    <Header/>
+      <Header>
+        
+      </Header>
+
+      <Post
+        author="Kaique Oliveira"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni dolores expedita voluptatum debitis suscipit inventore veritatis repudiandae exercitationem veniam! Sint perspiciatis similique ullam numquam ipsa officiis consectetur quisquam, aperiam neque."
+      />
     </>
   )
 }
 
-export default App
+
