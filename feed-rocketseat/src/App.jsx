53117a5ab6bae8d19c9ce09ec565bc75b4898ac2
Kaique Oliveira Antônio <kaique.oliveira@interfocus.com.br>
2023-06-17 13:11:22 -0300

    Orgabizado os imports

--- feed-rocketseat/src/App.jsx
+++ feed-rocketseat/src/App.jsx
@@ -1,9 +1,9 @@
 import { Header } from "./components/Header";
 import { Post } from "./components/Post";
+import { Sidebar } from "./components/SideBar";
 
 import styles from './App.module.css';
 import './global.css';
-import { Sidebar } from "./components/SideBar";
 
 export function App() {
   
