b41c7b58afcf98cbed8393f43bcfcf5e99d8052c
Kaique Oliveira Antônio <kaique.oliveira@interfocus.com.br>
2023-06-25 18:48:15 -0300

    Criado a função de deletar o comentário

--- feed-rocketseat/src/components/Comment.jsx
+++ feed-rocketseat/src/components/Comment.jsx
@@ -3,34 +3,39 @@ import { Avatar } from './Avatar';
 
 import styles from './Comment.module.css';
 
-export function Comment ({ content }) {
-  return (
-    <div className={styles.comment}>
-        <Avatar hasBorder={false} src="https://github.com/kaique-oliveira.png" />
+export function Comment ({ content, onDeleteComment }) {
 
-        <div className={styles.commentBox}>
-            <div className={styles.commentContent}>
-                <header>
-                    <div className={styles.authorAndTime}>
-                        <strong>Kaique Oliveira</strong>
-                        <time title='11 de Maio às 08:13h' dateTime='2022-05-11 08:13:30'>Cerca de 1h atrás</time>
-                    </div>
+    function handleDeleteComment(){
+        onDeleteComment(content);
+    }
 
-                    <button title="Deletar comentário">
-                        <Trash size={24}/>
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://github.com/kaique-oliveira.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Kaique Oliveira</strong>
+                            <time title='11 de Maio às 08:13h' dateTime='2022-05-11 08:13:30'>Cerca de 1h atrás</time>
+                        </div>
+
+                        <button onClick={handleDeleteComment} title="Deletar comentário">
+                            <Trash size={24}/>
+                        </button>
+                    </header>
+
+                    <p>{ content }</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp/>
+                        Aplaudir <span>20</span>
                     </button>
-                </header>
-
-                <p>{ content }</p>
+                </footer>
             </div>
-
-            <footer>
-                <button>
-                    <ThumbsUp/>
-                    Aplaudir <span>20</span>
-                </button>
-            </footer>
         </div>
-    </div>
-  )
+    )
 }
