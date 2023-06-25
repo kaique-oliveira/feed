600f564db7bb9accd813a1501b5e3fba9a5b0900
Kaique Oliveira Antônio <kaique.oliveira@interfocus.com.br>
2023-06-25 17:56:16 -0300

    Correção no id dos posts

--- feed-rocketseat/src/App.jsx
+++ feed-rocketseat/src/App.jsx
@@ -21,7 +21,7 @@ const posts = [
     publishedAt: new Date('2023-06-18 20:00:00')
   },
   {
-    id: 1,
+    id: 2,
     author: {
       avatarUrl: 'https://github.com/maykbrito.png',
       name: 'Mayk Brito',
